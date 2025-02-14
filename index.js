7ecf57a3f7c1318221b49ccab865f0101760ef2e
maggie januarie <livjanua@gmail.com>
2025-02-14 14:58:32 +0200

    Added basic Javascript.

---
+++ index.js
@@ -0,0 +1,12 @@
+function showMessage() {
+  alert("You Are Amazing ❣❣❣");
+}
+
+function subscribe() {
+  let email = prompt("Please enter your email");
+  if (email) {
+    alert("Thanks for subscribing!");
+  } else {
+    alert("Please enter email.");
+  }
+}
